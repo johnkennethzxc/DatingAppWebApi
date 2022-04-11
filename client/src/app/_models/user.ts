e5b8d6d24d9cd6ceeb36a1602f1f41bf981808a3
johnkennethzxc <bombanejohnkenneth01@gmail.com>
2022-04-11 12:55:17 +0800

    End of section 9

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -2,3 +2,26 @@ export interface User {
     username: string;
     token: string;
 }
+
+
+
+// let data: number | string = 0;
+
+// data = "string";
+
+// interface Car {
+//     color: string,
+//     model: string,
+//     topSpeed?: number
+// }
+
+// const car1: Car = {
+//     color: 'blue',
+//     model: 'BMW'
+// }
+
+// const car2: Car = {
+//     color: 'red',
+//     model: 'Mercedes',
+//     topSpeed: 100
+// }
